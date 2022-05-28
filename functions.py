5bac774c221d8c1e06158989a6cd67482b3186fe
Amos Wachira <samo@localhost.localdomain>
2022-05-28 16:24:29 +0300

    find the area of a circle

---
+++ functions.py
@@ -0,0 +1,6 @@
+import math
+def myfunc():
+    rad = float(input("Please input the radius of your choice "))
+    area = float(math.pi * rad*rad)
+    print ("the area of the circle is " , area)
+myfunc()
