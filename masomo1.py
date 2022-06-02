886fd3238a25c6052c941381d3d25b5b3ea55925
AMO15310 <wachiraamos402@gmail.com>
2022-06-03 01:26:12 +0300

    opens masomo1 on firefox if you have a web driver just update your credentials

--- masomo1.py
+++ masomo1.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 import time
 #password = Enter your password here
-#username = "Enter your regestration number here"
+#username = 'Enter your regestration number here'
 driver = webdriver.Firefox()
 driver.get("http://masomo1.kyu.ac.ke")
 element =driver.find_element_by_id("username")
@@ -12,3 +12,5 @@ element = driver.find_element_by_id("password")
 element.send_keys(password)
 time.sleep(2)
 element = driver.find_element_by_id("loginbtn").click()
+
+
