519ad9d526dde2ac812f3b791968c79119518a33
Amos Wachira <samo@localhost.localdomain>
2022-06-03 00:49:45 +0300

    open firefox if you have webdriver installed and login to your masomo1 portal just put your details

---
+++ masomo1.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+from selenium import webdriver
+import time
+#password = Enter your password here
+#username = "Enter your regestration number here"
+driver = webdriver.Firefox()
+driver.get("http://masomo1.kyu.ac.ke")
+element =driver.find_element_by_id("username")
+element.send_keys(username)
+time.sleep(2)
+element = driver.find_element_by_id("password")
+element.send_keys(password)
+time.sleep(2)
+element = driver.find_element_by_id("loginbtn").click()
+
+
